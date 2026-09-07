7913b13ff2e7c4a56c8d0f41430626da3d149998
amitvishwakarma1518 <amitvishwakarma2683@gmail.com>
2026-09-07 12:33:32 +0530

    assignment 2 completed

---
+++ Assignment 2/soltuion.py
@@ -0,0 +1,17 @@
+# solution1 
+# print("MySirG")
+
+
+#solution 2
+# print("MySirG \nEducation Services")
+
+# solution 3
+# print('"MySirG"')
+
+
+# solution 4
+# print(""" "Teacher's Day" """)
+# print(' "Teacher\'s Day" ')
+
+# solution 5
+print("\\n")
